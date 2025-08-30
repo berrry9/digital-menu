2b39e6afcd79f2f9d666cc23067f3f05c9ca2472
berry9 <155316713+berrry9@users.noreply.github.com>
2025-08-30 15:19:35 +0300

    Updated menuData.ts

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -45,7 +45,7 @@ export const menuItems: MenuItem[] = [
   {
     id: 'main-soup-1',
     name: 'Hungarian Soup',
-    description: 'Traditional red lentil soup with berbere spices and aromatic herbs',
+    description: 'Beef, Carrort, Cabbage',
     price: 8,
     image: 'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=600',
     category: 'noodles',
