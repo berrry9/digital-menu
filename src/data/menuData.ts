eceb9146790322f62aaa46928a1e6fb0cd181b7f
berry9 <155316713+berrry9@users.noreply.github.com>
2025-08-30 18:40:58 +0300

    Updated menuData.ts

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -442,7 +442,7 @@ export const menuItems: MenuItem[] = [
     subCategory: 'traditional',
     calories: 180,
     volume: '300g',
-    ingredients: ['Beef', 'Tomato', 'Onion', 'G.Papper', 'Injera', 'Butter', 'Injera', 'spices'],
+    ingredients: ['Beef', 'Tomato', 'Onion', 'G.Papper', 'Injera', 'Butter', 'Oil', 'spices'],
     available: true,
     quantity: 15,
     rating: 4.3,
