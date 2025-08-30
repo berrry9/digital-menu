35e193abeee308316843201487337bf6e7a854e9
berry9 <155316713+berrry9@users.noreply.github.com>
2025-08-30 18:12:36 +0300

    Updated menuData.ts

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -376,7 +376,7 @@ export const menuItems: MenuItem[] = [
     subCategory: 'rice',
     calories: 520,
     volume: '400g',
-    ingredients: ['Rice', 'Tomato', 'white wine', 'parmesan', 'butter', 'herbs'],
+    ingredients: ['Rice', 'Tomato', 'Onion', 'parmesan', 'butter', 'herbs'],
     available: true,
     quantity: 7,
     rating: 4.7,
