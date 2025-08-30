e67e7af713293a284a7486a1b8c8f766d9b28f78
berry9 <155316713+berrry9@users.noreply.github.com>
2025-08-30 16:48:12 +0300

    Updated menuData.ts

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -246,7 +246,7 @@ export const menuItems: MenuItem[] = [
     subCategory: 'spaghetti',
     calories: 620,
     volume: '350g',
-    ingredients: ['Tomato', 'Onion', 'Zikuni', 'Cabbage', 'Garlic', 'Carrot', 'Salt', 'Cabbage', 'Garlic', 'nutmeg'],
+    ingredients: ['Tomato', 'Onion', 'Zikuni', 'Cabbage', 'Garlic', 'Carrot', 'Salt', 'Oil', 'Garlic', 'nutmeg'],
     available: true,
     quantity: 7,
     rating: 4.5,
