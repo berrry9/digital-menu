b4cd95493e69617bc8540c361f2696a95f0c6692
berry9 <155316713+berrry9@users.noreply.github.com>
2025-09-03 03:48:28 +0300

    Updated menuData.ts

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -854,7 +854,7 @@ export const menuItems: MenuItem[] = [
     subCategory: 'sandwich',
     calories: 580,
     volume: '300g',
-    ingredients: ['Wheat flour', 'Beef', 'Lettuce', 'Egg', 'Cheese', 'Onion', 'Leek', 'Chili', 'Chicken', 'Potato', 'Mayonnaise', 'Kitchup', 'Cheese', 'mayo'],
+    ingredients: ['Wheat flour', 'Beef', 'Lettuce', 'Egg', 'Cheese', 'Onion', 'Leek', 'Chili', 'Chicken', 'Potato', 'Mayonnaise', 'Kitchup', 'Oil', 'Salt'],
     available: true,
     quantity: 15,
     rating: 4.5,
