96a325ebd0541a5b7a333a310a2e7681fc8cb04a
berry9 <155316713+berrry9@users.noreply.github.com>
2025-09-03 04:20:56 +0300

    Updated menuData.ts

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -968,7 +968,7 @@ export const menuItems: MenuItem[] = [
     subCategory: 'salad',
     calories: 280,
     volume: '280g',
-    ingredients: ['Onion', 'Chicken', 'Tomato', 'G.Pepper', 'cucumber', 'feta cheese', 'olives', 'tomatoes', 'cucumber', 'olive oil'],
+    ingredients: ['Onion', 'Chicken', 'Tomato', 'G.Pepper', 'Lettuce', 'feta cheese', 'olives', 'tomatoes', 'cucumber', 'olive oil'],
     available: true,
     quantity: 14,
     rating: 4.6,
