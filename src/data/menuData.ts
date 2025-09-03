2090bb640192c90255c631066b2f6b1041803f14
berry9 <155316713+berrry9@users.noreply.github.com>
2025-09-03 04:12:56 +0300

    Updated menuData.ts

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -918,7 +918,7 @@ export const menuItems: MenuItem[] = [
     subCategory: 'sandwich',
     calories: 480,
     volume: '260g',
-    ingredients: ['Wheat flour', 'chicken', 'romaine lettuce', 'caesar dressing', 'parmesan'],
+    ingredients: ['Wheat flour', 'Onion', 'romaine lettuce', 'caesar dressing', 'parmesan'],
     available: true,
     quantity: 14,
     rating: 4.4,
